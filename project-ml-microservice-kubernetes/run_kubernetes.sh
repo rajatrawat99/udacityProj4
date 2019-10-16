1dd186d526c08aa8695294697075ea0b1484c917
rajat <rajat@localhost.localdomain>
2019-10-15 20:52:50 -0400

    project changes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=rajatrawat88/projudacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run projudacity --image=rajatrawat88/projudacity:latest --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/projudacity 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,7 +5,8 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+# dockerpath=<your docker ID/path>:
+dockerpath=rajatrawat88/projudacity
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,4 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
